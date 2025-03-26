deeb388342edad8f61dcea030bc0f98aac78d448
GaYoung Hong <iamkaouo@gmail.com>
2025-03-26 15:42:30 +0900

    [init] 리액트 포트폴리오 프로젝트 (ver2) 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import DevPage from "./pages/DevPage";
+import MarketingPage from "./pages/MarketingPage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/dev" element={<DevPage />} />
+        <Route path="/marketing" element={<MarketingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
